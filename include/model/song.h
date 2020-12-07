2459427629c22900330d8c8450727b8784d3c062
Alden <aldenblamp@gmail.com>
2020-12-07 18:40:51 -0500

    Create song class

--- include/model/song.h
+++ include/model/song.h
@@ -5,11 +5,40 @@
 #ifndef FINAL_PROJECT_SONG_H
 #define FINAL_PROJECT_SONG_H
 
+#include "Resources.h"
+#include "cinder/gl/gl.h"
 #include "cinder/audio/audio.h"
+#include "cinder/app/App.h"
 
 
 namespace musicvis_model{
 
+using ci::audio::BufferPlayerNodeRef;
+using ci::audio::MonitorSpectralNodeRef;
+using ci::audio::MonitorSpectralNode;
+using ci::audio::SourceFileRef;
+using ci::audio::BufferRef;
+using std::vector;
+
+
+enum song_names { Amber };
+
+class Song{
+
+public:
+  Song(song_names name, size_t fft_size = 2048, size_t windowSize = 256);
+
+  void Play();
+
+  void Stop();
+
+  const vector<float>& GetFFTResult();
+
+private:
+  BufferPlayerNodeRef player_node_;
+  MonitorSpectralNodeRef monitor_node_;
+
+};
 
 
 }
--- src/model/song.cpp
+++ src/model/song.cpp
@@ -5,3 +5,34 @@
 
 #include <model/song.h>
 
+namespace musicvis_model{
+
+Song::Song(song_names name, size_t fft_size, size_t window_size) {
+  auto ctx = ci::audio::Context::master();
+
+  SourceFileRef sourceFile = ci::audio::load(ci::app::loadResource(FLUME_AMBER), ctx->getSampleRate());
+  BufferRef buffer = sourceFile->loadBuffer();
+  player_node_ = ctx->makeNode(new ci::audio::BufferPlayerNode(buffer));
+
+  MonitorSpectralNode::Format format = MonitorSpectralNode::Format().fftSize(fft_size).windowSize(window_size);
+  monitor_node_ = ctx->makeNode(new ci::audio::MonitorSpectralNode(format));
+
+  player_node_ >> monitor_node_;
+  player_node_ >> ctx->getOutput();
+  ctx->enable();
+}
+
+void Song::Play() {
+  player_node_->start();
+}
+
+void Song::Stop() {
+  player_node_->stop();
+}
+
+const vector<float> & Song::GetFFTResult() {
+  return monitor_node_->getMagSpectrum();
+}
+
+
+}
