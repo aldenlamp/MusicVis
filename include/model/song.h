a9ffa414ad27c9a1fb94319c7f0b0517ade593db
Alden <aldenblamp@gmail.com>
2020-12-09 23:31:08 -0500

    Reformatting and Add comments to Song class

--- include/model/song.h
+++ include/model/song.h
@@ -11,7 +11,7 @@
 #include "cinder/app/App.h"
 
 
-namespace musicvis_model{
+namespace musicvis_model {
 
 using ci::audio::BufferPlayerNodeRef;
 using ci::audio::MonitorSpectralNodeRef;
@@ -19,24 +19,76 @@ using ci::audio::MonitorSpectralNode;
 using ci::audio::SourceFileRef;
 using ci::audio::BufferRef;
 using std::vector;
+using glm::vec2;
 
+enum song_names {
+  Amber, FeelsLike, TimeAloneWith, Borderline, Nangs, Fkj
+};
 
-enum song_names { Amber };
+// Frames to use for blurring
+const double kHorizontalBlurFrame[]{0.1, 0.2, 0.4, 0.2, 0.1};
+const double kBackBlurFrame[]{0.3, 0.2, 0.15, 0.15, 0.1, 0.05, 0.05};
 
-class Song{
+class Song {
 
 public:
-  Song(song_names name, size_t fft_size = 2048, size_t windowSize = 256);
 
+  /**
+   * Creates a song
+   * @param bins the number of bins to analyze the song into
+   * @param name the name of the song to use
+   * @param fft_size the size (accuracy) of the fft
+   * @param window_size the size of the frame to analyze every time
+   * @param analysis_window the window to use from the analysis
+   */
+  Song(size_t bins, song_names name = Amber, size_t fft_size = 2048, size_t window_size = 1024,
+       vec2 analysis_window = vec2(8, 1024));
+
+  /**
+   * Plays the song or pauses the song depending on if it is currently playing
+   */
   void Play();
 
-  void Stop();
+  /**
+   * Stops the song and starts it from scratch
+   */
+  void Reset();
 
-  const vector<float>& GetFFTResult();
+  /**
+   * Getter for the analysis of the current position in the song
+   * @return
+   */
+  vector<double> GetBins();
+
+  /**
+   * Getter for the current volume of the song
+   * @return a float representing the volume
+   */
+  float GetVolume() const;
 
 private:
+
+  // Blurs one row of bins using the horizontal blur fram
+  void BlurSideways(vector<double> &bins);
+
+  // Blurs the given row of bins with the past few rows using the back blur frame
+  void BlurBackward(vector<double> &bins);
+
+  // Stores the last rows of bins for blurring
+  vector <vector<double>> last_bins_;
+
+  // Audio node to play and analyze the song
   BufferPlayerNodeRef player_node_;
   MonitorSpectralNodeRef monitor_node_;
+
+  // Analysis setting
+  size_t num_bins_;
+  vec2 analysis_window_;
+
+  // Info for pausing and playing
+  bool is_playing_;
+  double play_position_;
+
 
 };
 
--- src/model/song.cpp
+++ src/model/song.cpp
@@ -5,34 +5,180 @@
 
 #include <model/song.h>
 
-namespace musicvis_model{
+namespace musicvis_model {
 
-Song::Song(song_names name, size_t fft_size, size_t window_size) {
+Song::Song(size_t bins, song_names name, size_t fft_size, size_t window_size, vec2 analysis_window) {
+  // Loads the song resource from the song name
+  auto resource = ci::app::loadResource(FLUME_AMBER);
+  switch (name) {
+    case FeelsLike:
+      resource = ci::app::loadResource(FLWOGB);
+      break;
+    case TimeAloneWith:
+      resource = ci::app::loadResource(TIME_ALONE);
+      break;
+    case Borderline:
+      resource = ci::app::loadResource(BORDERLINE);
+      break;
+    case Nangs:
+      resource = ci::app::loadResource(NANGS);
+      break;
+    case Fkj:
+      resource = ci::app::loadResource(FKJ);
+      break;
+    default:
+      break;
+  }
+
+  is_playing_ = false;
+
+  // Sets up the player node
   auto ctx = ci::audio::Context::master();
-
-  SourceFileRef sourceFile = ci::audio::load(ci::app::loadResource(FLUME_AMBER), ctx->getSampleRate());
+  SourceFileRef sourceFile = ci::audio::load(resource, ctx->getSampleRate());
   BufferRef buffer = sourceFile->loadBuffer();
   player_node_ = ctx->makeNode(new ci::audio::BufferPlayerNode(buffer));
+  player_node_ >> ctx->getOutput();
 
+  // Sets up the monitor node
   MonitorSpectralNode::Format format = MonitorSpectralNode::Format().fftSize(fft_size).windowSize(window_size);
   monitor_node_ = ctx->makeNode(new ci::audio::MonitorSpectralNode(format));
-
   player_node_ >> monitor_node_;
-  player_node_ >> ctx->getOutput();
+
   ctx->enable();
+
+  // Saves analysis info
+  num_bins_ = bins;
+  analysis_window_ = analysis_window;
 }
 
 void Song::Play() {
-  player_node_->start();
+  if (!is_playing_) {
+    player_node_->start();
+    player_node_->seek(play_position_);
+    is_playing_ = true;
+  } else {
+    play_position_ = player_node_->getReadPosition();
+    player_node_->stop();
+    is_playing_ = false;
+  }
 }
 
-void Song::Stop() {
-  player_node_->stop();
+void Song::Reset() {
+  if (is_playing_) {
+    player_node_->stop();
+    is_playing_ = false;
+  }
+  play_position_ = 0;
 }
 
-const vector<float> & Song::GetFFTResult() {
-  return monitor_node_->getMagSpectrum();
+vector<double> Song::GetBins() {
+  // Gets raw fft data from the monitor node
+  const vector<float> &spect = monitor_node_->getMagSpectrum();
+
+  // Groups the data into bins logarithmically (to represent the data more like musical notes)
+
+  // Processes teh analysis window logarithmically
+  double start = log2(analysis_window_[0]);
+  double end = log2(analysis_window_[1]);
+  double div_size = (end - start) / num_bins_;
+
+  vector<double> bins = vector<double>();
+
+  // Groups the data into bins
+  double sum = 0;
+  size_t curr_bin = 0;
+  for (size_t i = analysis_window_[0]; i < analysis_window_[1]; i++) {
+    size_t window = (log2(i) - start) / div_size;
+    if (window >= curr_bin) {
+      curr_bin++;
+      bins.push_back(sum * sum);
+      sum = 0;
+    }
+    sum += spect[i];
+  }
+
+  // Blurs the bins sideways and backwards
+  BlurSideways(bins);
+  BlurBackward(bins);
+
+  return bins;
 }
 
+
+void Song::BlurSideways(vector<double> &bins) {
+  // Size of the blur on each side
+  size_t blur_size = (sizeof(kHorizontalBlurFrame) / sizeof(kHorizontalBlurFrame[0]) - 1) / 2;
+  vector<double> blurred;
+
+  for (size_t i = 0; i < bins.size(); i++) {
+    double new_value = 0;
+
+    // Handles the first few items before with a cut off blur filter
+    if (i < blur_size) {
+      double blur_sum = 0;
+      for (size_t j = 0; j < blur_size + i; j++) {
+        size_t blur_index = (blur_size - i) + j;
+        new_value += kHorizontalBlurFrame[blur_index] * bins[j];
+        blur_sum += kHorizontalBlurFrame[blur_index];
+      }
+      blurred.push_back(new_value / blur_sum);
+      continue;
+    }
+
+    // Handles normal cases and last few items
+    double blur_sum = 0;
+    for (size_t j = i - blur_size; j < ((i + blur_size > bins.size()) ? bins.size() : i + blur_size); j++) {
+      size_t blur_index = (j - i) + blur_size;
+      new_value += kHorizontalBlurFrame[blur_index] * bins[j];
+      blur_sum += kHorizontalBlurFrame[blur_index];
+    }
+    blurred.push_back(new_value / blur_sum);
+  }
+
+  // Updates given bins with blurred bins
+  for (size_t i = 0; i < bins.size(); i++) {
+    bins[i] = blurred[i];
+  }
+};
+
+void Song::BlurBackward(vector<double> &bins) {
+  // Size of the blur
+  size_t blur_size = sizeof(kBackBlurFrame) / sizeof(kBackBlurFrame[0]);
+  vector<double> blurred;
+
+  // Blurring with the last few bins
+  for (size_t i = 0; i < bins.size(); i++) {
+    double blur_sum = 0;
+    double value = 0;
+    for (size_t j = 0; j < last_bins_.size(); j++) {
+      if (j == 0) {
+        value += kBackBlurFrame[j] * bins[i];
+      } else if (j == 1) {
+        double greater = last_bins_[j - 1][i] > bins[i] ? last_bins_[j - 1][i] : bins[i];
+        value += kBackBlurFrame[j] * greater;
+      } else {
+        value += kBackBlurFrame[j] * last_bins_[j - 1][i];
+      }
+      blur_sum += kBackBlurFrame[j];
+    }
+    blurred.push_back(value / blur_sum);
+
+  }
+
+  // Updating the given bins with the blurred version
+  for (size_t i = 0; i < bins.size(); i++) {
+    bins[i] = blurred[i];
+  }
+
+  // Updating the last_bins vector
+  last_bins_.insert(last_bins_.begin(), blurred);
+  if (last_bins_.size() >= blur_size - 1) {
+    last_bins_.pop_back();
+  }
+}
+
+float Song::GetVolume() const {
+  return monitor_node_->getVolume();
+}
 
 }
