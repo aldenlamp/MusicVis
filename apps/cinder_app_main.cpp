a7af30f300af7cc81f8576e757beab95397826b0
Alden <aldenblamp@gmail.com>
2020-12-09 23:42:08 -0500

    Allow for resizing

--- apps/cinder_app_main.cpp
+++ apps/cinder_app_main.cpp
@@ -2,13 +2,7 @@
 // Created by alden lamp on 11/17/20.
 //
 
-//#include "cinder_app_main.h"
 #include <controller/musicvis_controller.h>
 
-using visualizer::MusicVisController;
-
-void prepareSettings(MusicVisController::Settings *settings){
-  settings->setResizable(false);
-}
-
-CINDER_APP(MusicVisController, ci::app::RendererGl, prepareSettings);
+using controller::MusicVisController;
+CINDER_APP(MusicVisController, ci::app::RendererGl);
